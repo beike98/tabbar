3185f85cb9e2ea884249d42829284c12a304232d
beike98 <492785718@qq.com>
2021-06-30 20:19:51 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
   <div id="app">
+    <router-view/>
     <tab-bar>
-      <tab-bar-item>
+      <tab-bar-item path="/home">
         <img slot="item-icon" src="./assets/img/tabbar/home.svg" />
+        <img slot="item-icon-active" src="./assets/img/tabbar/home-active.svg"/>
         <div slot="item-text">首页</div>
       </tab-bar-item>
-      <tab-bar-item>
+      <tab-bar-item path="category">
         <img slot="item-icon" src="./assets/img/tabbar/category.svg" />
+        <img slot="item-icon-active" src="./assets/img/tabbar/category-active.svg"/>
         <div slot="item-text">分类</div>
       </tab-bar-item>
-      <tab-bar-item>
+      <tab-bar-item path="shopcart">
         <img slot="item-icon" src="./assets/img/tabbar/shopcart.svg" />
+        <img slot="item-icon-active" src="./assets/img/tabbar/shopcart-active.svg"/>
         <div slot="item-text">购物车</div>
       </tab-bar-item>
-      <tab-bar-item>
+      <tab-bar-item path="profile">
         <img slot="item-icon" src="./assets/img/tabbar/profile.svg" />
+        <img slot="item-icon-active" src="./assets/img/tabbar/profile-active.svg"/>
         <div slot="item-text">我的</div>
       </tab-bar-item>
     </tab-bar>
--- src/components/tabbar/TabBarItem.vue
+++ src/components/tabbar/TabBarItem.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="tab-bar-item">
-    <slot name="item-icon"></slot>
-    <slot name="item-text"></slot>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :class="{active:isActive}"><slot name="item-text"></slot></div>
   </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    path: String
+  },
+  data() {
+    return {
+      isActive: true
+    }
+  },
+  methods: {
+    itemClick() {
+      this.$router.replace(this.path)
+    }
+  }
 }
 </script>
 
@@ -25,5 +38,9 @@ export default {
     margin-top: 3px;
     vertical-align: middle;
     margin-bottom: 2px;
+  }
+
+  .active {
+    color: #f00;
   }
 </style>
